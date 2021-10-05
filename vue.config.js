86fbed18a1f40945da1ee88851a1be1ec79c200d
r000909 <r000909@pc-000909-4.local>
2021-10-05 16:28:20 +0900

    add docs

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,30 @@
-// module.exports = {
-//   publicPath: '/github-pages-example-with-vue-router',
-//   outputDir: 'docs',
-//   filenameHashing: false,
-//   productionSourceMap: false,
-// };
+module.exports = {
+  publicPath: "/github-pages-example-with-vue-router",
+  outputDir: "docs",
+  filenameHashing: false,
+  productionSourceMap: false,
+  pages: {
+    index: {
+      entry: "./src/main.js",
+      template: "public/index.html",
+      filename: "app.html",
+    },
+  },
+  devServer: {
+    overlay: {
+      warnings: false,
+      errors: false,
+    },
+  },
+  chainWebpack: (config) => {
+    config.module
+      .rule("images")
+      .test(/\.(png|jpg|gif)$/i)
+      .use("url-loader")
+      .loader("url-loader")
+      .options({
+        limit: 8192,
+        name: "img/[name].[ext]",
+      });
+  },
+};
