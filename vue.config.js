cda34cae59bb1e9baa3b57b1d4aaeafe98d03833
r000909 <r000909@pc-000909-4.local>
2021-10-05 16:39:59 +0900

    add

--- vue.config.js
+++ vue.config.js
@@ -1,30 +1,6 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/vue-sample/" : "/",
+  publicPath: "/github-pages-cd2021",
   outputDir: "docs",
   filenameHashing: false,
   productionSourceMap: false,
-  pages: {
-    index: {
-      entry: "./src/main.js",
-      template: "public/index.html",
-      filename: "app.html",
-    },
-  },
-  devServer: {
-    overlay: {
-      warnings: false,
-      errors: false,
-    },
-  },
-  chainWebpack: (config) => {
-    config.module
-      .rule("images")
-      .test(/\.(png|jpg|gif)$/i)
-      .use("url-loader")
-      .loader("url-loader")
-      .options({
-        limit: 8192,
-        name: "img/[name].[ext]",
-      });
-  },
 };
